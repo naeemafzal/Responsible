77e3007c59bda1aedc2aea9dbd3f4f475dd1f622
Naeem Afzal <naeem4216@yahoo.com>
2018-08-14 23:03:45 +0100

    * Adding String Text Contain/Count Extention methods

--- Responsible.Utilities.Tests/StringTests.cs
+++ Responsible.Utilities.Tests/StringTests.cs
@@ -7,19 +7,20 @@ namespace Responsible.Utilities.Tests
     [TestClass]
     public class StringTests
     {
-        private static readonly List<string> Source = new List<string> { "Abc", "XyZ", "xyz" };
+        private static readonly List<string> ListSource = new List<string> { "Abc", "XyZ", "xyz" };
+        private static readonly string StringSource = "Abc abc xyz XYZ";
 
         [TestMethod]
         public void ListContainsText_CaseSensitive()
         {
-            var result = Source.ContainsText("abc", false);
+            var result = ListSource.ContainsText("abc");
             Assert.IsTrue(result, "List should contain the given text.");
         }
 
         [TestMethod]
         public void ListDoesNotContainText_CaseInSensitive()
         {
-            var result = Source.ContainsText("abc", true);
+            var result = ListSource.ContainsText("abc", true);
             Assert.IsFalse(result, "List should not contain the given text.");
         }
 
@@ -33,15 +34,50 @@ namespace Responsible.Utilities.Tests
         [TestMethod]
         public void ListContainsText_CaseSensitive_Count_2()
         {
-            var result = Source.ContainsTextCount("xyz", false);
+            var result = ListSource.ContainsTextCount("xyz");
             Assert.AreEqual(2, result, "The text count does not match.");
         }
 
         [TestMethod]
         public void ListContainsText_CaseSensitive_Count_1()
         {
-            var result = Source.ContainsTextCount("xyz", true);
+            var result = ListSource.ContainsTextCount("xyz", true);
             Assert.AreEqual(1, result, "The text count does not match.");
+        }
+
+        [TestMethod]
+        public void StringContainsText_CaseInsensitive_False()
+        {
+            var result = StringSource.ContainsText(false, "xyz", "ds");
+            Assert.IsFalse(result, "Result should be false.");
+        }
+
+        [TestMethod]
+        public void StringContainsText_CaseSensitive_False()
+        {
+            var result = StringSource.ContainsText(true, "xYz", "XYZ");
+            Assert.IsFalse(result, "Result should be false.");
+        }
+
+        [TestMethod]
+        public void StringContainsText_CaseSensitive_Count_0()
+        {
+            var result = StringSource.ContainsTextCount("xYz", true);
+            Assert.AreEqual(0, result, "Result should be 0.");
+        }
+
+        [TestMethod]
+        public void StringContainsText_CaseInSensitive_Count_2()
+        {
+            var result = StringSource.ContainsTextCount("xYz");
+            Assert.AreEqual(2, result, "Result should be 2.");
+        }
+
+        [TestMethod]
+        public void StringContainsText_CaseInsensitive_True()
+        {
+            var result = StringSource.ContainsText(false, "xyz", "ABC");
+            Assert.IsFalse(result, "Result should be true.");
         }
     }
 }
--- Responsible.Utilities/Extentions/StringExtentions.cs
+++ Responsible.Utilities/Extentions/StringExtentions.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Responsible.Utilities.Extentions
 {
@@ -62,6 +64,76 @@ namespace Responsible.Utilities.Extentions
             }
 
             return value.Equals(other);
+        }
+
+        /// <summary>
+        /// Checks if <see cref="string"/> contains the given item
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="searchText"></param>
+        /// <param name="caseSensitive">Define if the comparison is case sensitive</param>
+        /// <returns></returns>
+        public static bool ContainsText(this string value, string searchText, bool caseSensitive = false)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return false;
+            }
+
+            return caseSensitive ? value.Contains(searchText) : value.ToLower().Contains(searchText);
+        }
+
+        /// <summary>
+        /// Checks if <see cref="string"/> contains the all the given items
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="caseSensitive"></param>
+        /// <param name="predicates"></param>
+        /// <returns></returns>
+        public static bool ContainsText(this string value, bool caseSensitive, params string[] predicates)
+        {
+            //Returns false when predicates is null
+            if (predicates == null)
+            {
+                return false;
+            }
+
+            //Return true when the value is empty and all the predicates are also empty
+            if (string.IsNullOrWhiteSpace(value) && predicates.All(string.IsNullOrWhiteSpace))
+            {
+                return true;
+            }
+
+            //Returns false when value is empty
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return false;
+            }
+
+            return predicates.All(value.Contains);
+        }
+
+        /// <summary>
+        /// Get a count of <see cref="string"/> in an IEnumerable
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="searchText"></param>
+        /// <param name="caseSensitive">Define if the comparison is case sensitive</param>
+        /// <returns></returns>
+        public static int ContainsTextCount(this string value, string searchText, bool caseSensitive = false)
+        {
+            //If value is Empty and also the search text is empty then return 1
+            if (string.IsNullOrWhiteSpace(value) && string.IsNullOrWhiteSpace(searchText))
+            {
+                return 1;
+            }
+
+            //If value is empty then return 0
+            if (string.IsNullOrWhiteSpace(value)) return 0;
+
+            return caseSensitive
+                ? Regex.Matches(value, searchText).Count
+                : Regex.Matches(value.ToLower(), searchText.ToLower()).Count;
         }
 
         /// <summary>
