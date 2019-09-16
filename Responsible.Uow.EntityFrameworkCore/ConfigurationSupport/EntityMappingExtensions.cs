852def8c0b3f2605788f680f55f320282101540f
Naeem Afzal <naeem4216@yahoo.com>
2019-09-16 22:59:45 +0100

    EntityFrameworkCore version: 1.0.1

--- Responsible.Uow.EntityFrameworkCore/ConfigurationSupport/EntityMappingExtensions.cs
+++ Responsible.Uow.EntityFrameworkCore/ConfigurationSupport/EntityMappingExtensions.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Responsible.Uow.EntityFrameworkCore.ConfigurationSupport
 {
-    public interface IEntityMappingConfiguration<T> where T : class
-    {
-        void Map(EntityTypeBuilder<T> builder);
-    }
-
+    /// <summary>
+    /// Extension methods for Entity Configuration
+    /// </summary>
     public static class EntityMappingExtensions
     {
+        /// <summary>
+        /// An extension method to Register Entity and It's Configuration
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <typeparam name="TMapping"></typeparam>
+        /// <param name="builder"></param>
+        /// <returns></returns>
         public static ModelBuilder RegisterEntityMapping<TEntity, TMapping>(this ModelBuilder builder)
             where TMapping : IEntityMappingConfiguration<TEntity>
             where TEntity : class
---
+++ Responsible.Uow.EntityFrameworkCore/ConfigurationSupport/IEntityMappingConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Responsible.Uow.EntityFrameworkCore.ConfigurationSupport
+{
+    /// <summary>
+    /// An interface to implement build the Entity Configuration
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEntityMappingConfiguration<T> where T : class
+    {
+        /// <summary>
+        /// Method implemented to create Entity builder
+        /// </summary>
+        /// <param name="builder"></param>
+        void Map(EntityTypeBuilder<T> builder);
+    }
+}
